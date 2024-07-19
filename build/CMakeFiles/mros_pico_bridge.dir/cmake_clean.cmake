851fcf537130a8a61b1552077c5d06bf3a161025
mbotinfo <mbotinfo>
2024-07-19 14:35:29 -0400

    first commit

---
+++ build/CMakeFiles/mros_pico_bridge.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mros_pico_bridge.dir/src/main.cpp.o"
+  "mros_pico_bridge"
+  "mros_pico_bridge.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mros_pico_bridge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
