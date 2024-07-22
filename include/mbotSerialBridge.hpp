1f3affefc79331deb25e0471bdd7a32deb1a13de
Broderick <broderio@umich.edu>
2024-07-22 09:09:35 -0400

    implements serial parsing

---
+++ include/mbotSerialBridge.hpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <memory>
+#include <mutex>
+
+#include <stdio.h>
+#include <stdint.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/time.h>
+
+#include "utils.hpp"
+#include "mros/node.hpp"
+#include "mros/publisher.hpp"
+#include "mros/subscriber.hpp"
+#include "mros/console.hpp"
+
+#include "messages/mbot_msgs/encoders.hpp"
+#include "messages/mbot_msgs/mbotImu.hpp"
+#include "messages/mbot_msgs/motorPwm.hpp"
+#include "messages/mbot_msgs/motorVel.hpp"
+#include "messages/mbot_msgs/pose2D.hpp"
+#include "messages/mbot_msgs/timestamp.hpp"
+#include "messages/mbot_msgs/twist2D.hpp"
+
+#define ROS_HEADER_LENGTH 7
+#define ROS_FOOTER_LENGTH 1
+#define ROS_PKG_LENGTH  (ROS_HEADER_LENGTH + ROS_FOOTER_LENGTH) //length (in bytes) of ros packaging (header and footer)
+
+class MbotSerialBridge
+{
+    public:
+        MbotSerialBridge();
+        ~MbotSerialBridge();
+        void run();
+
+    private:
+        mros::Node node;
+
+        // Publishers
+        std::shared_ptr<mros::Publisher> odometryPub;
+        std::shared_ptr<mros::Publisher> imuPub;
+        std::shared_ptr<mros::Publisher> encodersPub;
+        std::shared_ptr<mros::Publisher> mbotVelPub;
+        std::shared_ptr<mros::Publisher> motorVelPub;
+        std::shared_ptr<mros::Publisher> motorPwmPub;
+
+        // Subscribers
+        std::shared_ptr<mros::Subscriber> timesyncSub;
+        std::shared_ptr<mros::Subscriber> odometryResetSub;
+        std::shared_ptr<mros::Subscriber> encodersResetSub;
+        std::shared_ptr<mros::Subscriber> motorPwmCmdSub;
+        std::shared_ptr<mros::Subscriber> motorVelCmdSub;
+        std::shared_ptr<mros::Subscriber> mbotVelCmdSub;
+
+        // Callbacks
+        void timesyncCallback(const mbot_msgs::Timestamp &msg);
+        void odometryResetCallback(const mbot_msgs::Pose2D &msg);
+        void encodersResetCallback(const mbot_msgs::Encoders &msg);
+        void motorPwmCmdCallback(const mbot_msgs::MotorPwm &msg);
+        void motorVelCmdCallback(const mbot_msgs::MotorVel &msg);
+        void mbotVelCmdCallback(const mbot_msgs::Twist2D &msg);
+
+        // Handlers for serial message parsing
+        bool readHeader(uint8_t* headerData);
+        bool validateHeader(uint8_t* headerData);
+        bool readMessage(uint8_t* msgDataSerialized, uint16_t msgLen, char* topicMsgDataChecksum);
+        bool validateMessage(uint8_t* headerData, uint8_t* msgDataSerialized, uint16_t msgLen, char topicMsgDataChecksum);
+        void handleMessage(uint8_t* msgDataSerialized, uint16_t msgLen, uint16_t topicId);
+        uint8_t checksum(uint8_t* addends, int len);
+
+        int serialDevice;
+        std::mutex serialMutex;
+};
---
+++ src/mbotSerialBridge.cpp
@@ -0,0 +1,254 @@
+#include "mbotSerialBridge.hpp"
+
+MbotSerialBridge::MbotSerialBridge() : node("serial_bridge", URI("0.0.0.0", MEDIATOR_PORT_NUM))
+{
+    serialDevice = open("/dev/mbot_lcm", O_RDWR);
+
+    odometryPub = node.advertise<mbot_msgs::Pose2D>("odometry", 10);
+    imuPub = node.advertise<mbot_msgs::MbotImu>("imu", 10);
+    encodersPub = node.advertise<mbot_msgs::Encoders>("encoders", 10);
+    mbotVelPub = node.advertise<mbot_msgs::Twist2D>("mbot_vel", 10);
+    motorVelPub = node.advertise<mbot_msgs::MotorVel>("motor_vel", 10);
+    motorPwmPub = node.advertise<mbot_msgs::MotorPwm>("motor_pwm", 10);
+
+    timesyncSub = node.subscribe<mbot_msgs::Timestamp>("timesync", 10, std::bind(&MbotSerialBridge::timesyncCallback, this, std::placeholders::_1));
+    odometryResetSub = node.subscribe<mbot_msgs::Pose2D>("odometry_reset", 10, std::bind(&MbotSerialBridge::odometryResetCallback, this, std::placeholders::_1));
+    encodersResetSub = node.subscribe<mbot_msgs::Encoders>("encoders_reset", 10, std::bind(&MbotSerialBridge::encodersResetCallback, this, std::placeholders::_1));
+    motorPwmCmdSub = node.subscribe<mbot_msgs::MotorPwm>("motor_pwm_cmd", 10, std::bind(&MbotSerialBridge::motorPwmCmdCallback, this, std::placeholders::_1));
+    motorVelCmdSub = node.subscribe<mbot_msgs::MotorVel>("motor_vel_cmd", 10, std::bind(&MbotSerialBridge::motorVelCmdCallback, this, std::placeholders::_1));
+    mbotVelCmdSub = node.subscribe<mbot_msgs::Twist2D>("mbot_vel_cmd", 10, std::bind(&MbotSerialBridge::mbotVelCmdCallback, this, std::placeholders::_1));
+}
+
+MbotSerialBridge::~MbotSerialBridge()
+{
+    std::lock_guard<std::mutex> lock(serialMutex);
+    close(serialDevice);
+}
+
+void MbotSerialBridge::timesyncCallback(const mbot_msgs::Timestamp &msg)
+{
+    std::lock_guard<std::mutex> lock(serialMutex);
+    write(serialDevice, &msg, sizeof(msg));
+}
+
+void MbotSerialBridge::odometryResetCallback(const mbot_msgs::Pose2D &msg)
+{
+    std::lock_guard<std::mutex> lock(serialMutex);
+    write(serialDevice, &msg, sizeof(msg));
+}
+
+void MbotSerialBridge::encodersResetCallback(const mbot_msgs::Encoders &msg)
+{
+    std::lock_guard<std::mutex> lock(serialMutex);
+    write(serialDevice, &msg, sizeof(msg));
+}
+
+void MbotSerialBridge::motorPwmCmdCallback(const mbot_msgs::MotorPwm &msg)
+{
+    std::lock_guard<std::mutex> lock(serialMutex);
+    write(serialDevice, &msg, sizeof(msg));
+}
+
+void MbotSerialBridge::motorVelCmdCallback(const mbot_msgs::MotorVel &msg)
+{
+    std::lock_guard<std::mutex> lock(serialMutex);
+    write(serialDevice, &msg, sizeof(msg));
+}
+
+void MbotSerialBridge::mbotVelCmdCallback(const mbot_msgs::Twist2D &msg)
+{
+    std::lock_guard<std::mutex> lock(serialMutex);
+    write(serialDevice, &msg, sizeof(msg));
+}
+
+bool MbotSerialBridge::readHeader(uint8_t *headerData)
+{
+    unsigned char triggerVal = 0x00;
+    int rc = 0x00;
+    while (triggerVal != 0xff && node.ok() && rc != 1)
+    {
+        rc = read(serialDevice, &triggerVal, 1);
+        if (rc < 0)
+        {
+            return -1;
+        }
+    }
+    headerData[0] = triggerVal;
+
+    rc = read(serialDevice, &headerData[1], ROS_HEADER_LENGTH - 1);
+    if (rc < 0)
+    {
+        return -1;
+    }
+
+    return (rc == ROS_HEADER_LENGTH - 1);
+}
+
+bool MbotSerialBridge::validateHeader(uint8_t *headerData)
+{
+    bool validHeader = (headerData[1] == 0xfe);
+    uint8_t cs1Addends[2] = {headerData[2], headerData[3]};
+    uint8_t csMsgLen = checksum(cs1Addends, 2);
+    validHeader = validHeader && (csMsgLen == headerData[4]);
+
+    return validHeader;
+}
+
+bool MbotSerialBridge::readMessage(uint8_t *msgDataSerialized, uint16_t msgLen, char *topicMsgDataChecksum)
+{
+    int rc = read(serialDevice, msgDataSerialized, msgLen);
+    if (rc < 0)
+    {
+        return -1;
+    }
+    bool validMsg = (rc == msgLen);
+
+    rc = read(serialDevice, topicMsgDataChecksum, 1);
+    if (rc < 0)
+    {
+        return -1;
+    }
+    validMsg = validMsg && (rc == 1);
+
+    return validMsg;
+}
+
+bool MbotSerialBridge::validateMessage(uint8_t *headerData, uint8_t *msgDataSerialized, uint16_t msgLen, char topicMsgDataChecksum)
+{
+    uint8_t *cs2Addends = new uint8_t[msgLen + 2];
+    cs2Addends[0] = headerData[5];
+    cs2Addends[1] = headerData[6];
+    std::memcpy(&cs2Addends[2], msgDataSerialized, msgLen);
+
+    uint8_t csTopicMsgData = checksum(cs2Addends, msgLen + 2);
+    bool validMsg = (csTopicMsgData == topicMsgDataChecksum);
+
+    delete[] cs2Addends;
+
+    return validMsg;
+}
+
+void MbotSerialBridge::handleMessage(uint8_t *msgDataSerialized, uint16_t msgLen, uint16_t topicId)
+{
+    std::string msgStr((char*)msgDataSerialized);
+
+    switch (topicId)
+    {
+    case MBOT_ODOMETRY:
+    {
+        mbot_msgs::Pose2D msg;
+        msg.decode(msgStr);
+        odometryPub->publish(msg);
+        break;
+    }
+    case MBOT_IMU:
+    {
+        mbot_msgs::MbotImu msg;
+        msg.decode(msgStr);
+        imuPub->publish(msg);
+        break;
+    }
+    case MBOT_ENCODERS:
+    {
+        mbot_msgs::Encoders msg;
+        msg.decode(msgStr);
+        encodersPub->publish(msg);
+        break;
+    }
+    case MBOT_VEL:
+    {
+        mbot_msgs::Twist2D msg;
+        msg.decode(msgStr);
+        mbotVelPub->publish(msg);
+        break;
+    }
+    case MBOT_MOTOR_VEL:
+    {
+        mbot_msgs::MotorVel msg;
+        msg.decode(msgStr);
+        motorVelPub->publish(msg);
+        break;
+    }
+    case MBOT_MOTOR_PWM:
+    {
+        mbot_msgs::MotorPwm msg;
+        msg.decode(msgStr);
+        motorPwmPub->publish(msg);
+        break;
+    }
+    default:
+        break;
+    }
+}
+
+uint8_t MbotSerialBridge::checksum(uint8_t* addends, int len) {
+    //takes in an array and sums the contents then checksums the array
+    int sum = 0;
+    for (int i = 0; i < len; i++) {
+        sum += addends[i];
+    }
+    return 255 - ( ( sum ) % 256 );
+}
+
+void MbotSerialBridge::run()
+{
+    node.spin(true);
+
+    uint8_t headerData[ROS_HEADER_LENGTH];
+    headerData[0] = 0x00;
+    while (node.ok())
+    {
+        // Read the header and check if we lost serial connection
+        int headerStatus = readHeader(headerData);
+        if (headerStatus < 0)
+        {
+            fprintf(stderr, "[ERROR] Serial device is not available, exiting thread to attempt reconnect...\n");
+            break; // Break the loop if the device is not available
+        }
+
+        bool validHeader = (headerStatus == 1);
+        if (validHeader)
+        {
+            validHeader = validateHeader(headerData);
+        }
+
+        if (validHeader)
+        {
+            uint16_t msgLen = ((uint16_t)headerData[3] << 8) + (uint16_t)headerData[2];
+            uint16_t topicId = ((uint16_t)headerData[6] << 8) + (uint16_t)headerData[5];
+            uint8_t msgDataSerialized[msgLen];
+
+            int avail = 0;
+            ioctl(serialDevice, FIONREAD, &avail);
+            while (avail < (msgLen + 1) && node.ok())
+            {
+                usleep(1000);
+                ioctl(serialDevice, FIONREAD, &avail);
+            }
+            if (!node.ok())
+            {
+                break;
+            }
+
+            // Read the message and check if we lost serial connection
+            char topicMsgDataChecksum = 0;
+            int msgStatus = readMessage(msgDataSerialized, msgLen, &topicMsgDataChecksum);
+            if (msgStatus < 0)
+            {
+                fprintf(stderr, "[ERROR] Serial device is not available, exiting thread to attempt reconnect...\n");
+                break; // Break the loop if the device is not available
+            }
+
+            bool validMsg = (msgStatus == 1);
+            if (validMsg)
+            {
+                validMsg = validateMessage(headerData, msgDataSerialized, msgLen, topicMsgDataChecksum);
+                if (validMsg)
+                {
+                    handleMessage(msgDataSerialized, msgLen, topicId);
+                }
+            }
+        }
+        headerData[0] = 0x00;
+    }
+}
